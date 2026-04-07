899c9b6453f84e1b807a513d7a830bd54165288f
raghunadhbr-git <raghunadhbr@gmail.com>
2026-04-07 18:56:14 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5003))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 # venv\Scripts\activate
